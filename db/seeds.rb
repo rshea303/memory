334800281baddaabee343cd83bc6dd026163601b
Rich Shea <richcshea@gmail.com>
2015-09-09 09:57:02 -0600

    update seed file to include facts for every mammal

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,23 +13,41 @@ def generate_cards
   lion.facts << Fact.create(fact: "lion fact 2")
   lion.facts << Fact.create(fact: "lion fact 3")
 
-  Card.create(title: "elephant",
+  elephant = Card.create(title: "elephant",
               url: 'http://media1.santabanta.com/full1/Animals/Elephants/elephants-9a.jpg')
-  
-  Card.create(title: "penguin",
+  elephant.facts << Fact.create(fact: "elephant fact 1")
+  elephant.facts << Fact.create(fact: "elephant fact 2")
+  elephant.facts << Fact.create(fact: "elephant fact 3")
+ 
+  penguin = Card.create(title: "penguin",
               url: 'http://upload.wikimedia.org/wikipedia/commons/0/07/Emperor_Penguin_Manchot_empereur.jpg')
+  penguin.facts << Fact.create(fact: "penguin fact 1")
+  penguin.facts << Fact.create(fact: "penguin fact 2")
+  penguin.facts << Fact.create(fact: "penguin fact 3")
 
-  Card.create(title: "gorilla",
+  gorilla = Card.create(title: "gorilla",
               url: 'http://assets.nydailynews.com/polopoly_fs/1.1466956!/img/httpImage/image.jpg_gen/derivatives/article_970/dallas-anti-social-ape.jpg')
+  gorilla.facts << Fact.create(fact: "gorilla fact 1")
+  gorilla.facts << Fact.create(fact: "gorilla fact 2")
+  gorilla.facts << Fact.create(fact: "gorilla fact 3")
 
-  Card.create(title: "orangutan",
+  orangutan = Card.create(title: "orangutan",
               url: 'http://www.orangutan.com/wp-content/uploads/2012/12/sn-orangutans1.jpg')
-  
-  Card.create(title: "antelope",
+  orangutan.facts << Fact.create(fact: "orangutan fact 1")
+  orangutan.facts << Fact.create(fact: "orangutan fact 2")
+  orangutan.facts << Fact.create(fact: "orangutan fact 3")
+ 
+  antelope = Card.create(title: "antelope",
               url: 'http://passnownow.com/wp-content/uploads/2015/07/Animal8.jpg')
+  antelope.facts << Fact.create(fact: "antelope fact 1")
+  antelope.facts << Fact.create(fact: "antelope fact 2")
+  antelope.facts << Fact.create(fact: "antelope fact 3")
 
-  Card.create(title: "hippo",
+  hippo = Card.create(title: "hippo",
               url: 'http://s.hswstatic.com/gif/hippo-sunscreen-1.jpg')
+  hippo.facts << Fact.create(fact: "hippo fact 1")
+  hippo.facts << Fact.create(fact: "hippo fact 2")
+  hippo.facts << Fact.create(fact: "hippo fact 3")
 
   puts "...cards completed..."
 end
