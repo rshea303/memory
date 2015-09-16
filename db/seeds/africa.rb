384375347bb9d581c11b5236119cbe5ef5838493
Rich Shea <richcshea@gmail.com>
2015-09-16 14:11:36 -0600

    seed africa and fix asia

--- db/seeds/africa.rb
+++ db/seeds/africa.rb
@@ -1,1 +1,65 @@
-africa = Game.create(name: "Africa")
+def generate_africa_animals
+  africa = Game.create(name: "Africa")
+  puts "generating #{africa.name} cards"
+
+  rhino = Card.create(title: "rhino",
+              url: 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQvabjMkA2chC2qPu7kXE4YA9FCGq-KxXINIQO3yJhlgpFEaFK0')
+  rhino.facts << Fact.create(fact: "")
+  rhino.facts << Fact.create(fact: "")
+  rhino.facts << Fact.create(fact: "")
+  puts "#{rhino.title}"
+  
+  cheetah = Card.create(title: "cheetah",
+              url: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRpx9qK7K4X2LsJCKBVkeciSEqOzDrTkm0KURTDWQJS6UGlSOITOg')
+  cheetah.facts << Fact.create(fact: "")
+  cheetah.facts << Fact.create(fact: "")
+  cheetah.facts << Fact.create(fact: "")
+  puts "#{cheetah.title}"
+
+  gorilla = Card.create(title: "gorilla",
+              url: 'http://assets.nydailynews.com/polopoly_fs/1.1466956!/img/httpImage/image.jpg_gen/derivatives/article_970/dallas-anti-social-ape.jpg')
+  gorilla.facts << Fact.create(fact: "Gorillas are large apes.")
+  gorilla.facts << Fact.create(fact: "Gorillas are native to Africa.")
+  gorilla.facts << Fact.create(fact: "Gorillas are the world's largest primate.")
+  puts "#{gorilla.title}"
+
+  hippo = Card.create(title: "hippo",
+              url: 'http://s.hswstatic.com/gif/hippo-sunscreen-1.jpg')
+  hippo.facts << Fact.create(fact: "The name hippopotamus means 'river horse.'")
+  hippo.facts << Fact.create(fact: "Althought hippos might look a little chubby, they can easily outrun a human.")
+  hippo.facts << Fact.create(fact: "Hippos give birth in water.")
+  puts "#{hippo.title}"
+
+  crocodile = Card.create(title: "crocodile",
+              url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3KL5Z7ro2OBHnyzWVwvYGnCKHCLzLR0XgAQ8XsvDT9XI0xbiA9Q')
+  crocodile.facts << Fact.create(fact: "")
+  crocodile.facts << Fact.create(fact: "")
+  crocodile.facts << Fact.create(fact: "")
+  puts "#{crocodile.title}"
+
+  giraffe = Card.create(title: "giraffe",
+              url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRT0PyPrE-p-SMrNzSg_EKPzvy981hcxo-tCvqOPH3n0UzF9-46')
+  giraffe.facts << Fact.create(fact: "The giraffe is the tallest land animal in the world.  They grow to 16-18 feet tall.")
+  giraffe.facts << Fact.create(fact: "Giraffes sleep less than 2 hours each day.")
+  giraffe.facts << Fact.create(fact: "Giraffes consume over 100 pounds of leaves and twigs each day.")
+  puts "#{giraffe.title}"
+
+  zebra = Card.create(title: "zebra",
+              url: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcT3ojFX-iLfLRKIanU7FC5jEqVI_x3_LQJH2kpPoUVWh1EvH6pVPw')
+  zebra.facts << Fact.create(fact: "Every zebra has a unique pattern of black and white stripes.")
+  zebra.facts << Fact.create(fact: "Zebras stand up while sleeping.")
+  zebra.facts << Fact.create(fact: "Zebras run side to side when being chased by a predator.")
+  puts "#{zebra.title}"
+
+  lion = Card.create(title: "lion",
+              url: 'http://globe-views.com/dcim/dreams/lion/lion-05.jpg')
+  lion.facts << Fact.create(fact: "The roar of a lion can be heard from 8 kilometes (5.0 miles) away.")
+  lion.facts << Fact.create(fact: "In the wild, lions rest for about 20 hours a day.")
+  lion.facts << Fact.create(fact: "The heaviest lion on record weighed an amazing 826lbs!")
+  puts "#{lion.title}"
+
+  africa.cards.push(rhino, cheetah, beaver, elk, crocodile, grizzly_bear, alligator, lion)
+  puts "----------"
+end
+
+generate_africa_animals
--- db/seeds/asia.rb
+++ db/seeds/asia.rb
@@ -9,12 +9,12 @@ def generate_asia_animals
   tiger.facts << Fact.create(fact: "Tigers have been known to reach speeds up to 40 mph.")
   puts "#{tiger.title}"
   
-  lion = Card.create(title: "lion",
-              url: 'http://globe-views.com/dcim/dreams/lion/lion-05.jpg')
-  lion.facts << Fact.create(fact: "The roar of a lion can be heard from 8 kilometes (5.0 miles) away.")
-  lion.facts << Fact.create(fact: "In the wild, lions rest for about 20 hours a day.")
-  lion.facts << Fact.create(fact: "The heaviest lion on record weighed an amazing 826lbs!")
-  puts "#{lion.title}"
+  panda_bear = Card.create(title: "panda bear",
+              url: '')
+  panda_bear.facts << Fact.create(fact: "")
+  panda_bear.facts << Fact.create(fact: "")
+  panda_bear.facts << Fact.create(fact: "")
+  puts "#{panda_bear.title}"
 
   elephant = Card.create(title: "elephant",
               url: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRZGFaOgYlvzCklqqHNT4P5-zcOPuRF6wB8g_-LZ_44T0gaJZel')
@@ -23,19 +23,19 @@ def generate_asia_animals
   elephant.facts << Fact.create(fact: "Female elephants are called cows.")
   puts "#{elephant.title}"
  
-  zebra = Card.create(title: "zebra",
-              url: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcT3ojFX-iLfLRKIanU7FC5jEqVI_x3_LQJH2kpPoUVWh1EvH6pVPw')
-  zebra.facts << Fact.create(fact: "Every zebra has a unique pattern of black and white stripes.")
-  zebra.facts << Fact.create(fact: "Zebras stand up while sleeping.")
-  zebra.facts << Fact.create(fact: "Zebras run side to side when being chased by a predator.")
-  puts "#{zebra.title}"
+  camel = Card.create(title: "camel",
+              url: 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSWxRxo3LnvYmRErbWQlCdCS8yNn1-Bxl4FIsCNj8tIgAnfbhojjw')
+  camel.facts << Fact.create(fact: "")
+  camel.facts << Fact.create(fact: "")
+  camel.facts << Fact.create(fact: "")
+  puts "#{camel.title}"
 
-  gorilla = Card.create(title: "gorilla",
-              url: 'http://assets.nydailynews.com/polopoly_fs/1.1466956!/img/httpImage/image.jpg_gen/derivatives/article_970/dallas-anti-social-ape.jpg')
-  gorilla.facts << Fact.create(fact: "Gorillas are large apes.")
-  gorilla.facts << Fact.create(fact: "Gorillas are native to Africa.")
-  gorilla.facts << Fact.create(fact: "Gorillas are the world's largest primate.")
-  puts "#{gorilla.title}"
+  red_panda = Card.create(title: "red panda",
+              url: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTtSlyLoPJBRB7CnL8uv0bINeH2RM-sW7CwyiV0eApGjzWx7mqqGg')
+  red_panda.facts << Fact.create(fact: "")
+  red_panda.facts << Fact.create(fact: "")
+  red_panda.facts << Fact.create(fact: "")
+  puts "#{red_panda.title}"
 
   orangutan = Card.create(title: "orangutan",
               url: 'http://www.orangutan.com/wp-content/uploads/2012/12/sn-orangutans1.jpg')
@@ -44,21 +44,21 @@ def generate_asia_animals
   orangutan.facts << Fact.create(fact: "Orangutans make umbrellas for themselves out of big leaves when it rains.")
   puts "#{orangutan.title}"
  
-  giraffe = Card.create(title: "giraffe",
-              url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRT0PyPrE-p-SMrNzSg_EKPzvy981hcxo-tCvqOPH3n0UzF9-46')
-  giraffe.facts << Fact.create(fact: "The giraffe is the tallest land animal in the world.  They grow to 16-18 feet tall.")
-  giraffe.facts << Fact.create(fact: "Giraffes sleep less than 2 hours each day.")
-  giraffe.facts << Fact.create(fact: "Giraffes consume over 100 pounds of leaves and twigs each day.")
-  puts "#{giraffe.title}"
+  volture = Card.create(title: "volture",
+              url: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRr4slrV3U44pBCtKuz0s71sHTQbElxkCk8G1uhTv2lKdJc6xUm')
+  volture.facts << Fact.create(fact: "")
+  volture.facts << Fact.create(fact: "")
+  volture.facts << Fact.create(fact: "")
+  puts "#{volture.title}"
 
-  hippo = Card.create(title: "hippo",
-              url: 'http://s.hswstatic.com/gif/hippo-sunscreen-1.jpg')
-  hippo.facts << Fact.create(fact: "The name hippopotamus means 'river horse.'")
-  hippo.facts << Fact.create(fact: "Althought hippos might look a little chubby, they can easily outrun a human.")
-  hippo.facts << Fact.create(fact: "Hippos give birth in water.")
-  puts "#{hippo.title}"
+  red_river_hog = Card.create(title: "red river hog",
+              url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlxvu0-PjRh43vPl8-8i42gNmTjb6nZFoGanPjPT2waDIkxodu')
+  red_river_hog.facts << Fact.create(fact: "")
+  red_river_hog.facts << Fact.create(fact: "")
+  red_river_hog.facts << Fact.create(fact: "")
+  puts "#{red_river_hog.title}"
 
-  asia.cards.push(tiger, lion, elephant, zebra, gorilla, orangutan, giraffe, hippo)
+  asia.cards.push(tiger, panda_bear, elephant, camel, red_panda, orangutan, volture, red_river_hog)
   puts "----------"
 end
 
