31dfeb74bae2cd28dd2b5dd6f66235dcf079cc16
Rich Shea <richcshea@gmail.com>
2015-09-16 12:01:24 -0600

    add north america and antarctica

--- db/seeds/antarctica.rb
+++ db/seeds/antarctica.rb
@@ -1,1 +1,65 @@
-antarctica = Game.create(name: "Antarctica")
+def generate_antarctica_animals
+  antarctica = Game.create(name: "Antarctica")
+  puts "generating #{antarctica.name} cards"
+
+  penguin = Card.create(title: "penguin",
+              url: 'http://www.coolantarctica.com/Antarctica%20fact%20file/anim1.jpg')
+  penguin.facts << Fact.create(fact: "")
+  penguin.facts << Fact.create(fact: "")
+  penguin.facts << Fact.create(fact: "")
+  puts "#{penguin.title}"
+  
+  leopard_seal = Card.create(title: "leopard seal",
+              url: 'http://www.coolantarctica.com/Antarctica%20fact%20file/anim8.jpg')
+  leopard_seal.facts << Fact.create(fact: "")
+  leopard_seal.facts << Fact.create(fact: "")
+  leopard_seal.facts << Fact.create(fact: "")
+  puts "#{leopard_seal.title}"
+
+  killer_whale = Card.create(title: "killer whale",
+              url: 'http://www.coolantarctica.com/Antarctica%20fact%20file/anim9.jpg')
+  killer_whale.facts << Fact.create(fact: "")
+  killer_whale.facts << Fact.create(fact: "")
+  killer_whale.facts << Fact.create(fact: "")
+  puts "#{killer_whale.title}"
+ 
+  albatross = Card.create(title: "albatross",
+              url: 'https://www.bas.ac.uk/wp-content/uploads/2015/04/4d119bcdf3efec6738ae742006acbacd_1428765983-400x250.jpg')
+  albatross.facts << Fact.create(fact: "")
+  albatross.facts << Fact.create(fact: "")
+  albatross.facts << Fact.create(fact: "")
+  puts "#{albatross.title}"
+
+  sperm_whale = Card.create(title: "sperm whale",
+              url: 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRt_FfU1Ce86g0K_t4dAv2c_B0Hggw4N5hsv5I0yBaTxz2OK7g13A')
+  sperm_whale.facts << Fact.create(fact: "")
+  sperm_whale.facts << Fact.create(fact: "")
+  sperm_whale.facts << Fact.create(fact: "")
+  puts "#{sperm_whale.title}"
+
+  humpback_whale = Card.create(title: "humpback whale",
+              url: 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQ5cJOdzz8DXE8Gwh4zG-qbd0iHjWCP3MSci6jdH2h7L8OdfTxn-A')
+  humpback_whale.facts << Fact.create(fact: "")
+  humpback_whale.facts << Fact.create(fact: "")
+  humpback_whale.facts << Fact.create(fact: "")
+  puts "#{humpback_whale.title}"
+ 
+  elephant_seal = Card.create(title: "elephant seal",
+              url: 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQq_aFBmg3U6bTlOq0h-KMUAM5CQTCAiybdxtN3wMZFahlssO294A')
+  elephant_seal.facts << Fact.create(fact: "")
+  elephant_seal.facts << Fact.create(fact: "")
+  elephant_seal.facts << Fact.create(fact: "")
+  puts "#{elephant_seal.title}"
+
+  snow_petrel = Card.create(title: "snow petrel",
+              url: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSyqDnVLNFS2I80j2z4UQqBnkkM4bHnkmuAiMR08IH1SOzZarIk')
+  snow_petrel.facts << Fact.create(fact: "")
+  snow_petrel.facts << Fact.create(fact: "")
+  snow_petrel.facts << Fact.create(fact: "")
+  puts "#{snow_petrel.title}"
+
+  antarctica.cards.push(penguin, leopard_seal, killer_whale, albatross, sperm_whale, humpback_whale, elephant_seal, mountain_lion)
+  puts "----------"
+end
+
+generate_antarctica_animals
--- db/seeds/north_america.rb
+++ db/seeds/north_america.rb
@@ -1,1 +1,65 @@
-north_america = Game.create(name: "North America")
+def generate_north_america_animals
+  north_america = Game.create(name: "North America")
+  puts "generating #{north_america.name} cards"
+
+  wolverine = Card.create(title: "wolverine",
+              url: '')
+  wolverine.facts << Fact.create(fact: "")
+  wolverine.facts << Fact.create(fact: "")
+  wolverine.facts << Fact.create(fact: "")
+  puts "#{wolverine.title}"
+  
+  chipmunk = Card.create(title: "chipmunk",
+              url: '')
+  chipmunk.facts << Fact.create(fact: "")
+  chipmunk.facts << Fact.create(fact: "")
+  chipmunk.facts << Fact.create(fact: "")
+  puts "#{chipmunk.title}"
+
+  beaver = Card.create(title: "beaver",
+              url: '')
+  beaver.facts << Fact.create(fact: "")
+  beaver.facts << Fact.create(fact: "")
+  beaver.facts << Fact.create(fact: "")
+  puts "#{beaver.title}"
+ 
+  elk = Card.create(title: "elk",
+              url: '')
+  elk.facts << Fact.create(fact: "")
+  elk.facts << Fact.create(fact: "")
+  elk.facts << Fact.create(fact: "")
+  puts "#{elk.title}"
+
+  bald_eagle = Card.create(title: "bald eagle",
+              url: '')
+  bald_eagle.facts << Fact.create(fact: "")
+  bald_eagle.facts << Fact.create(fact: "")
+  bald_eagle.facts << Fact.create(fact: "")
+  puts "#{bald_eagle.title}"
+
+  grizzly_bear = Card.create(title: "grizzly bear",
+              url: '')
+  grizzly_bear.facts << Fact.create(fact: "")
+  grizzly_bear.facts << Fact.create(fact: "")
+  grizzly_bear.facts << Fact.create(fact: "")
+  puts "#{grizzly_bear.title}"
+ 
+  alligator = Card.create(title: "alligator",
+              url: '')
+  alligator.facts << Fact.create(fact: "")
+  alligator.facts << Fact.create(fact: "")
+  alligator.facts << Fact.create(fact: "")
+  puts "#{alligator.title}"
+
+  mountain_chipmunk = Card.create(title: "mountain lion",
+              url: '')
+  mountain_chipmunk.facts << Fact.create(fact: "")
+  mountain_chipmunk.facts << Fact.create(fact: "")
+  mountain_chipmunk.facts << Fact.create(fact: "")
+  puts "#{mountain_chipmunk.title}"
+
+  north_america.cards.push(wolverine, chipmunk, beaver, elk, bald_eagle, grizzly_bear, alligator, mountain_lion)
+  puts "----------"
+end
+
+generate_north_america_animals
